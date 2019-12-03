87a32fd39b986e187d55dc9132c6c2f09f4761a5
KrustyKrabbyBoi <47988114+KrustyKrabbyBoi@users.noreply.github.com>
2019-12-03 09:26:02 -0800

    updated from 8bitworkshop.com

--- game.c
+++ game.c
@@ -24,7 +24,7 @@
 
 /*{pal:"nes",layout:"nes"}*/
 const char PALETTE[32] = { 
-  0x03,			// screen color
+  0x01,			// screen color
 
   0x11,0x30,0x27,0x0,	// background palette 0
   0x1c,0x20,0x2c,0x0,	// background palette 1
@@ -37,9 +37,17 @@ const char PALETTE[32] = {
   0x0d,0x27,0x2a	// sprite palette 3
 };
 
-char map[16*14] = {
-  
-};
+int y = 160;
+
+int yv = 0;
+
+char pad;
+
+byte s = 0;
+
+
+
+bool start = false;
 
 // setup PPU and tables
 void setup_graphics() {
@@ -47,17 +55,53 @@ void setup_graphics() {
   oam_clear();
   // set palette colors
   pal_all(PALETTE);
+} 
+
+void die() {
+  start = false;
+  yv = 0;
+  y = 160;
 }
 
 void main(void)
 {
   setup_graphics();
-  // draw message  
-  vram_adr(NTADR_A(0,0));
+  
+  vram_adr(NTADR_A(1,1));
+  
+  vrambuf_clear();
+  set_vram_update(updbuf);
+  
+  vram_fill(0x09, 30);
+  
+  vram_adr(NTADR_A(0,27));
+  
+  vram_fill(0x09, 31);
   
   // enable rendering
   ppu_on_all();
   // infinite loop
   while(1) {
+    pad = pad_poll(0);
+    
+    if (start) {
+    	if (yv < 40) yv += 2;
+   	y += yv;
+    	if (pad & PAD_A) yv = -30;
+    	oam_spr(32, y/10, 0xb1, 1, 0);
+      
+      	if (y > 2040 || y < 160) die();
+    } else if (pad & PAD_START) {
+      start = true;
+      s++;
+    }
+    
+    vrambuf_clear();
+    
+    vrambuf_put(NTADR_A(2,3), "0", 1);
+    
+    
+    
+    ppu_wait_frame();
   }
 }
